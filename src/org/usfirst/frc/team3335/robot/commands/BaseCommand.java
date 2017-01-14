e48787821df4056fd687f3dc96b97cbbd652dad5
jkmacy <joyousjake.jm@gmail.com>
2017-01-14 11:22:34 -0600

    Added two base commands and subsystems.

---
+++ src/org/usfirst/frc/team3335/robot/commands/BaseCommand.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team3335.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ * Created by jacob on 1/14/17.
+ */
+public class BaseCommand extends Command{
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
+}
---
+++ src/org/usfirst/frc/team3335/robot/subsystems/BaseSystem.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team3335.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * Created by jacob on 1/14/17.
+ */
+public class BaseSystem extends Subsystem{
+    @Override
+    protected void initDefaultCommand() {
+        
+    }
+}
