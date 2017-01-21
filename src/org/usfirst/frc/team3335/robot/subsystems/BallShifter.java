da798d5ccd2a225468faeafacf3a0cdabbb40706
jkmacy <joyousjake.jm@gmail.com>
2017-01-21 14:40:29 -0600

    Finished up basic ball shifting command.
    Also removed Solenoid classes.

--- src/org/usfirst/frc/team3335/robot/subsystems/BallShifter.java
+++ src/org/usfirst/frc/team3335/robot/subsystems/BallShifter.java
@@ -1,10 +1,25 @@
 package org.usfirst.frc.team3335.robot.subsystems;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class BallShifter extends Subsystem implements LoggableSubsystem{
 
-	
+	private DoubleSolenoid solenoid;
+
+	public BallShifter() {
+		solenoid = new DoubleSolenoid(1, 2);
+		setSolenoidValue(DoubleSolenoid.Value.kOff);
+	}
+
+	public void setSolenoidValue(DoubleSolenoid.Value value) {
+		solenoid.set(value);
+	}
+
+	public DoubleSolenoid.Value getSolenoidValue() {
+		return solenoid.get();
+	}
+
 	@Override
 	protected void initDefaultCommand() {
 		// TODO Auto-generated method stub
