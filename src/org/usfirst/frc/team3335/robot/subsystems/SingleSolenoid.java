9a105f2dcb6ca301b8421d9e53f5011d89276697
jkmacy <joyousjake.jm@gmail.com>
2017-01-21 12:27:06 -0600

    Initlialized a solenoid in SingleSolenoid

--- src/org/usfirst/frc/team3335/robot/subsystems/SingleSolenoid.java
+++ src/org/usfirst/frc/team3335/robot/subsystems/SingleSolenoid.java
@@ -1,11 +1,18 @@
 package org.usfirst.frc.team3335.robot.subsystems;
 
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  * Created by jacob on 1/21/17.
  */
 public class SingleSolenoid extends Subsystem implements LoggableSubsystem {
+
+    private Solenoid solenoid;
+
+    public SingleSolenoid() {
+        solenoid = new Solenoid(0);
+    }
 
 
     @Override
