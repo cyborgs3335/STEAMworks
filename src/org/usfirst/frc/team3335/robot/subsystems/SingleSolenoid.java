61de816c5ae67adfc1dc2889f390795e58a9eedc
jkmacy <joyousjake.jm@gmail.com>
2017-01-21 11:52:26 -0600

    Added two Solenoid subsystems.

---
+++ src/org/usfirst/frc/team3335/robot/subsystems/SingleSolenoid.java
@@ -0,0 +1,20 @@
+package org.usfirst.frc.team3335.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * Created by jacob on 1/21/17.
+ */
+public class SingleSolenoid extends Subsystem implements LoggableSubsystem {
+
+
+    @Override
+    protected void initDefaultCommand() {
+
+    }
+
+    @Override
+    public void log() {
+
+    }
+}
