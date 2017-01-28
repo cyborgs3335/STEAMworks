2b8e58d4effdca67ddb07fbed670d1562d32aad2
jkmacy <joyousjake.jm@gmail.com>
2017-01-28 17:07:47 -0600

    Added NavX subsytem which returns the yaw. WIP.

---
+++ src/org/usfirst/frc/team3335/robot/subsystems/NavX.java
@@ -0,0 +1,30 @@
+package org.usfirst.frc.team3335.robot.subsystems;
+
+import com.kauailabs.navx.frc.AHRS;
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * Created by jacob on 1/28/17.
+ */
+public class NavX extends Subsystem implements LoggableSubsystem {
+    private AHRS navX;
+
+    public NavX() {
+        navX = new AHRS(I2C.Port.kMXP);
+    }
+
+    @Override
+    protected void initDefaultCommand() {
+
+    }
+
+    public double getYaw() {
+        return navX.getYaw();
+    }
+
+    @Override
+    public void log() {
+
+    }
+}
