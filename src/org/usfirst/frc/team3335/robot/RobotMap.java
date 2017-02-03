922a75191894ff38adb891472196db162a947b19
Brian Macy <macybk@gmail.com>
2017-02-02 20:50:24 -0600

    Add simple ball shooter subsystem

--- src/org/usfirst/frc/team3335/robot/RobotMap.java
+++ src/org/usfirst/frc/team3335/robot/RobotMap.java
@@ -22,5 +22,5 @@ public class RobotMap {
     public static final int DRIVE_TRAIN_BACK_LEFT = 1;
     public static final int DRIVE_TRAIN_BACK_RIGHT = 3;
 	public static final int CLIMBING_MOTOR = 5;
-	public static final int BALL_SHOOTER = 6000;
+	public static final int BALL_SHOOTER_MOTOR = 6000;
 }
---
+++ src/org/usfirst/frc/team3335/robot/subsystems/BallShooter.java
@@ -0,0 +1,34 @@
+package org.usfirst.frc.team3335.robot.subsystems;
+
+import org.usfirst.frc.team3335.robot.RobotMap;
+
+import com.ctre.CANTalon;
+
+import edu.wpi.first.wpilibj.CANSpeedController;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class BallShooter extends Subsystem implements LoggableSubsystem {
+
+	private CANTalon motor;
+	
+	public BallShooter() {
+		motor = new CANTalon(RobotMap.BALL_SHOOTER_MOTOR);
+		//motor.setControlMode();
+	}
+
+	public void set(double value) {
+		motor.set(value);
+	}
+
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void log() {
+		// TODO Auto-generated method stub
+		
+	}
+}
