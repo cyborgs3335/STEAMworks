51aa2d626a91303691f4f9f67914be450b9c5213
Alyssa Cassity <alyssarocks2563@gmail.com>
2017-02-18 15:41:45 -0600

    add double solinoid and method for switching its position

--- src/org/usfirst/frc/team3335/robot/subsystems/BallShooter.java
+++ src/org/usfirst/frc/team3335/robot/subsystems/BallShooter.java
@@ -6,15 +6,29 @@ import org.usfirst.frc.team3335.robot.RobotMap;
 import com.ctre.CANTalon;
 
 import edu.wpi.first.wpilibj.CANSpeedController;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class BallShooter extends Subsystem implements LoggableSubsystem {
 
 	private CANTalon motor;
 	
+	private DoubleSolenoid solenoid;
+	
 	public BallShooter() {
 		motor = new CANTalon(RobotMap.BALL_SHOOTER_MOTOR);
 		//motor.setControlMode();
+	}
+	public void switchPos(){
+		switch(solenoid.get()){
+			case kOff: case kReverse: 
+				solenoid.set(Value.kForward);
+				break;
+			case kForward: 
+				solenoid.set(Value.kReverse);
+				break;
+		}
 	}
 
 	public void set(double value) {
