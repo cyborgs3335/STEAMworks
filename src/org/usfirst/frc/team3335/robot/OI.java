20dd60a794343662704960eabf06445eb90567b9
Brian Macy <macybk@gmail.com>
2017-03-09 22:59:35 -0600

    Refactor OI class to use variables for the button numbers.  Set driver button numbers to "Kevin mode" or "Duncan mode".

--- src/org/usfirst/frc/team3335/robot/OI.java
+++ src/org/usfirst/frc/team3335/robot/OI.java
@@ -5,9 +5,7 @@ import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import org.usfirst.frc.team3335.robot.commands.*;
-import org.usfirst.frc.team3335.robot.commands.autonomous.AutoSteerDriveToPeg;
-import org.usfirst.frc.team3335.robot.commands.autonomous.AutoTurnToPeg;
-import org.usfirst.frc.team3335.robot.subsystems.Intake;
+import org.usfirst.frc.team3335.robot.commands.autonomous.*;
 
 /**
  * This class is the glue that binds the controls on the physical operator
@@ -21,50 +19,92 @@ public class OI {
         joystick = new Joystick(0);
         joystick2 = new Joystick(1);
 
-        JoystickButton gateControlUp = addButton(getJoystick(), 3, "Gate Up");
+        boolean kevinMode = true; // Kevin
+        //boolean kevinMode = false; // Duncan
+        int bDefault = -1;
+        int bGateUp = bDefault;
+        int bGateDown = bDefault;
+        int bFlapperUp = bDefault;
+        int bFlapperDown = bDefault;
+        int bShiftLow = bDefault;
+        int bShiftHigh = bDefault;
+        int bDriveForward = bDefault;
+        int bDriveBackward = bDefault;
+        int bClimberUpSlow = 5; // Left Button
+        int bClimberUpFast = 6; // Right Button
+        int bClimberDown = 7; // "Back"
+        int bShooter = 3; // Right Trigger
+        int bIntakeOn = 1; // A
+        int bIntakeOff = 2; // B
+
+        if (kevinMode) {
+        	// Joystick 1
+        	bGateUp = 3;
+        	bGateDown = 2;
+        	bFlapperUp = 5;
+        	bFlapperDown = 4;
+        	bShiftLow = 7;
+        	bShiftHigh = 6;
+        	bDriveForward = 8;
+        	bDriveBackward = 9;
+        } else {
+        	// Joystick 1
+        	bGateUp = 3;
+        	bGateDown = 2;
+        	bFlapperUp = 6;
+        	bFlapperDown = 7;
+        	bShiftLow = 4;
+        	bShiftHigh = 5;
+        	bDriveForward = bDefault;
+        	bDriveBackward = bDefault;
+        }
+
+        // Gate
+        JoystickButton gateControlUp = addButton(getJoystick(), bGateUp, "Gate Up");
         gateControlUp.whenPressed(new GateControl(true));
-        JoystickButton gateControlDown = addButton(getJoystick(), 2, "Gate Down");
+        JoystickButton gateControlDown = addButton(getJoystick(), bGateDown, "Gate Down");
         gateControlDown.whenPressed(new GateControl(false));
 
-        JoystickButton driveForward = addButton(getJoystick(), 6, "Drive Forward");
+        // Flapper
+        JoystickButton flapperUp = addButton(getJoystick(), bFlapperUp, "Flapper Up");
+        flapperUp.whenPressed(new FlapperControl(false));
+        JoystickButton flapperDown = addButton(getJoystick(), bFlapperDown, "Flapper Down");
+        flapperDown.whenPressed(new FlapperControl(true));
+
+        // Drive Mode: Front is Forward vs Back is Forward
+        JoystickButton driveForward = addButton(getJoystick(), bDriveForward, "Drive Forward");
         driveForward.whenPressed(new SetDirection(true));
-        JoystickButton driveBackward = addButton(getJoystick(), 7, "Drive Backward");
+        JoystickButton driveBackward = addButton(getJoystick(), bDriveBackward, "Drive Backward");
         driveBackward.whenPressed(new SetDirection(false));
 
-        JoystickButton ballShiftHigh = addButton(getJoystick(), 5, "Ball Shifter High");
+        // Ball Shifter
+        JoystickButton ballShiftHigh = addButton(getJoystick(), bShiftHigh, "Ball Shifter High");
         ballShiftHigh.whenPressed(new BallShiftHigh());
-        JoystickButton ballShiftLow = addButton(getJoystick(), 4, "Ball Shifter Low");
+        JoystickButton ballShiftLow = addButton(getJoystick(), bShiftLow, "Ball Shifter Low");
         ballShiftLow.whenPressed(new BallShiftLow());
 
         // Rope climber
-        //JoystickButton climbUp = addButton(getJoystick(), 2, "Rope Climber");
-        JoystickButton climbUp = addButton(getJoystick2(), 2, "Rope Climber Slow");
-        JoystickButton climbDown = addButton(getJoystick(), 8, "Rope Climber");
-        climbUp.whenPressed(new Climb(false, 0.5));
-        climbUp.whenReleased(new Climb(true, 0));
+        JoystickButton climbUpSlow = addButton(getJoystick2(), bClimberUpSlow, "Rope Climber Slow");
+        climbUpSlow.whenPressed(new Climb(false, 0.5));
+        climbUpSlow.whenReleased(new Climb(true, 0));
+        JoystickButton climbUpFast = addButton(getJoystick2(), bClimberUpFast, "Rope Climber Fast");
+        climbUpFast.whenPressed(new Climb(false, 1));
+        climbUpFast.whenReleased(new Climb(true, 0));
+        JoystickButton climbDown = addButton(getJoystick2(), bClimberDown, "Rope Climber Down");
         climbDown.whenPressed(new Climb(false, -1));
         climbDown.whenReleased(new Climb(true, 0));
-//        JoystickButton climbUp = addButton(getJoystick2(), 2, "Rope Climber");
-        JoystickButton climbUp2 = addButton(getJoystick(), 9, "Rope Climber Fast");
-        climbUp2.whenPressed(new Climb(false, 1));
-        climbUp2.whenReleased(new Climb(true, 0));
 
-        JoystickButton shootBalls = addButton(getJoystick(), 1, "Shoot Balls");
+        // Shooter
+        JoystickButton shootBalls = addButton(getJoystick2(), bShooter, "Shoot Balls");
         shootBalls.whenPressed(new ShootBalls(false, -1));
         shootBalls.whenReleased(new ShootBalls(true, 0));
 
         // Intake
-        JoystickButton intakeOn = addButton(getJoystick(), 10, "Intake On");
+        JoystickButton intakeOn = addButton(getJoystick2(), bIntakeOn, "Intake On");
         intakeOn.whenPressed(new IntakeBalls(false, -0.5));
-        JoystickButton intakeOff = addButton(getJoystick(), 11, "Intake Off");
+        JoystickButton intakeOff = addButton(getJoystick2(), bIntakeOff, "Intake Off");
         intakeOff.whenPressed(new IntakeBalls(true, 0));
-        
-        // Flapper
-        JoystickButton flapperUp = addButton(getJoystick2(), 7, "Flapper Up");
-        flapperUp.whenPressed(new FlapperControl(false));
-        JoystickButton flapperDown = addButton(getJoystick2(), 8, "Flapper Down");
-        flapperDown.whenPressed(new FlapperControl(true));
-        
+
         // Additional commands to add to dashboard
 		SmartDashboard.putData("AutoTurnToPeg", new AutoTurnToPeg());
 		SmartDashboard.putData("AutoSteerDriveToPeg", new AutoSteerDriveToPeg(60, 0.7, 9));
