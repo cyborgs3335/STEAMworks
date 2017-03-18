dc3e8dcac2b784e12440faccf0c5d5e4e6966220
jkmacy <joyousjake.jm@gmail.com>
2017-03-18 00:36:00 -0500

    Modify AutoPlaceDropGear to close gate and push forward before backing away, in effort to make sure gear is near back of peg.

--- src/org/usfirst/frc/team3335/robot/commands/autonomous/AutoPlaceDropGear.java
+++ src/org/usfirst/frc/team3335/robot/commands/autonomous/AutoPlaceDropGear.java
@@ -8,14 +8,18 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class AutoPlaceDropGear extends CommandGroup {
 	public AutoPlaceDropGear(double distInitial, double turnAngle, double distPost, double distFinal,
 			double speed) {
-		// TODO Auto-generated constructor stub
 		addSequential(new AutoDelay(0));
 		addSequential(new BallShiftLow());
 		addSequential(new AutoDriveToPeg(distInitial, speed));
 		//addSequential(new AutoTurnToPeg(turnAngle));
 		//addSequential(new AutoDriveToPeg(distPost));
-		addSequential(new GateControl(false));
+
+		// Open gate, pause, backup 12 in, close gate, move forward 10 in, then backup distFinal
+		addSequential(new GateControl(false)); // open
 		addSequential(new AutoDelay(1000));
+		addSequential(new AutoDriveToPeg(-12, speed));
+		addSequential(new GateControl(true)); // close
+		addSequential(new AutoDriveToPeg(10, speed));
 		addSequential(new AutoDriveToPeg(distFinal, speed));
 	}
 }
