41decb42550c9eeeeaa6372040533e01f6eda3ff
jkmacy <joyousjake.jm@gmail.com>
2017-04-07 15:39:35 -0500

    Reduced delay in between gate and eject

--- src/org/usfirst/frc/team3335/robot/commands/GearControl.java
+++ src/org/usfirst/frc/team3335/robot/commands/GearControl.java
@@ -7,12 +7,12 @@ public class GearControl extends CommandGroup {
 	public GearControl(boolean f) {
 		if (f) {
 			addSequential(new GearEjectorControl(f));
-			addSequential(new Delay(250));
+			addSequential(new Delay(150));
 			addSequential(new GateControl(f));
 		}
 		else {
 			addSequential(new GateControl(f));
-			addSequential(new Delay(250));
+			addSequential(new Delay(150));
 			addSequential(new GearEjectorControl(f));
 		}
 	}
